39019a5cd54de03b17596f4a57ec4b98b5164788
Hugo-CASTELL <hugo.castell@outlook.fr>
2023-12-20 19:05:13 +0100

    React Architecture

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.tsx";
+
+function App() {
+  return (
+    <Router>
+        <Routes>
+            <Route path={"/"} Component={HomePage} />
+        </Routes>
+    </Router>
+  )
+}
+
+export default App
