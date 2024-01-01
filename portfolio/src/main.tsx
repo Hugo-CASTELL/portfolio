0f218328ebc93b4d978cb7e51398b80712808335
Hugo-CASTELL <hugo.castell@outlook.fr>
2024-01-01 20:59:23 +0100

    Fix unhoverable header

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -9,13 +9,13 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <div className={"absolute z-20 top-0 inset-x-0 flex justify-center overflow-hidden pointer-events-none"}>
             <div title={"beams"} className={"relative"}>
                 <picture>
-                    <source srcSet="assets/img/beams2.avif" type="image/avif"/>
+                    <source srcSet="assets/img/beams.avif" type="image/avif"/>
                     <img src="assets/img/beams.webp" alt="Background beams"/>
                 </picture>
             </div>
         </div>
 
-        <Header className={"fixed top-0 w-full bg-transparent"}/>
+        <Header className={"fixed z-10 top-0 w-full bg-transparent"}/>
 
         <div className="absolute overflow-hidden w-full">
             <div className={"relative mx-0 w-full"}>
